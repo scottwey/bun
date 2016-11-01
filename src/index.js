fe1c31386bbb5a728b6d7a8159265613d3765363
Scott Wey <wey.scott@gmail.com>
2016-10-31 22:20:27 -0700

    add animated, css only loading

--- src/index.js
+++ src/index.js
@@ -2,19 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import IndexContainer from './containers/Index';
-import root from './ducks';
 import './index.css';
 
 import configureStore from './store';
 
 const store = configureStore();
-
-if (module.hot) {
-  module.hot.accept('./ducks', () => {
-    const nextRoot = require('./ducks');
-    store.replaceReducer(nextRoot);
-  });
-}
 
 ReactDOM.render(
   <Provider store={store}>
