2881b58223db97cbcff6d92ebb7286ba9368779a
Scott Wey <Scott Wey>
2016-11-25 01:50:30 -0800

    persist redux state in local storage, throttling algorithm

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import IndexContainer from './containers/Index';
 import './index.css';
+import configureStore, { loadState, saveState, throttle } from './store';
 
-import configureStore from './store';
-
-const store = configureStore();
+const persistedState = loadState();
+console.log(persistedState);
+const store = configureStore(persistedState);
+const throttleState = {};
+store.subscribe(throttle.bind(throttleState, () => { saveState(store.getState()); }, 500));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store.js
+++ src/store.js
@@ -1,8 +1,44 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import root from './ducks';
+import rootReducer from './ducks';
 
 const enhancer = applyMiddleware(thunk);
 export default function configureStore(initialState = {}) {
-  return createStore(root, initialState, enhancer);
+  return createStore(rootReducer, initialState, enhancer);
+}
+
+export const loadState = () => {
+  try {
+    const serializedState = localStorage.getItem('bunQuestRedux');
+    if (serializedState === null) {
+      return {};
+    }
+    return JSON.parse(serializedState);
+  } catch (err) {
+    return {};
+  }
+};
+
+export const saveState = (state) => {
+  try {
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('bunQuestRedux', serializedState);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export function throttle(fxn, delay) {
+  const self = this;
+  self.now = new Date();
+  if (!self.prev) {
+    self.prev = [];
+    self.prev.push(self.now);
+  }
+  const earliest = Math.min(...self.prev);
+  if (self.now - earliest > delay) {
+    fxn();
+    self.prev = [];
+  }
+  self.prev.push(self.now);
 }
